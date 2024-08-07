8f17dafe722420e3bdd5e3697c0c339259847518
Mehdi <mahdi@DESKTOP-0VJJLML>
2024-08-07 16:58:51 +0330

    send post

--- main.py
+++ main.py
@@ -238,7 +238,7 @@ async def Button_click(update:Update , context:ContextTypes.DEFAULT_TYPE) :
 
     elif text=="ارسال پست به تمام کاربران":
         user_id =update.message.from_user.id
-        if user_id != ADMIN_CHAT_ID.__init__:
+        if user_id != int(ADMIN_CHAT_ID):
             print(user_id,ADMIN_CHAT_ID)
             await update.message.reply_text('شما مجوز ارسال پست را ندارید.')
             return
