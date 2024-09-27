5c1c51d8391c05755508c14e1cc9889c99246ed3
Mehdi <mahdi@DESKTOP-0VJJLML>
2024-09-27 16:17:38 +0330

    article

--- main.py
+++ main.py
@@ -193,37 +193,14 @@ async def send_article(context: CallbackContext):
 
 
 
-    abst = random_article['bib'].get('abstract', 'No abstract available')
+    abstract = random_article['bib'].get('abstract', 'No abstract available')
 
-    res = f"📚 {random_article['bib']['title']}\n" \
-             f"👨‍🔬 Author(s): {', '.join(random_article['bib']['author'])}\n" \
-             f"📅 Year: {random_article['bib'].get('pub_year', 'Unknown')}\n" \
-             f"🔗 [Link to Article]({random_article.get('pub_url', '#')})\n\n\n" \
-             f"Abstract:\n{abst}\n\n" \
-             "--"
-    try:
-        genai.configure(api_key=gen_token)
-
-        model = genai.GenerativeModel("gemini-1.5-flash")
-        content = f"""لطفا این مقاله رو به شکل خیلی خوب و با جزيیات بررسی کن و برداشت هات رو به شکل زبان عامیانه فارسی به‌طور کامل شرح بده بطور علمی و دقیق با فرمولها و دلایل حرفه‌ای و دقیقا توضیح بده این مقاله رو.
-
-لینک مقاله و خلاصه‌ای ازش: {res}
-دقت کن حدود 8 تا 12 خط باشه توضیحاتت
-لطفا انتهای پست هم رفرنس بزار 
-"""
-        response = await model.generate_content(content)
-        abstract = response.replace("#", "")
-
-    except Exception as e:
-        print(f"ERROR : {e}")
-
-        
     result = f"📚 {random_article['bib']['title']}\n" \
              f"👨‍🔬 Author(s): {', '.join(random_article['bib']['author'])}\n" \
              f"📅 Year: {random_article['bib'].get('pub_year', 'Unknown')}\n" \
              f"🔗 [Link to Article]({random_article.get('pub_url', '#')})\n\n\n" \
              f"Abstract:\n{abstract}\n\n" \
-             "--"
+                "--"
 
     try:
         # ارسال به کاربران
@@ -234,6 +211,33 @@ async def send_article(context: CallbackContext):
         # ارسال به کانال آرشیو
         await context.bot.send_message(chat_id=TARGET, text=result, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
     
+    except Exception as e:
+        print(f"ERROR : {e}")
+
+
+    try:
+        genai.configure(api_key=gen_token)
+
+        model = genai.GenerativeModel("gemini-1.5-flash")
+        content = f"""لطفا این مقاله رو به شکل خیلی خوب و با جزيیات بررسی کن و برداشت هات رو به شکل زبان عامیانه فارسی به‌طور کامل شرح بده بطور علمی و دقیق با فرمولها و دلایل حرفه‌ای و دقیقا توضیح بده این مقاله رو.
+
+لینک مقاله و خلاصه‌ای ازش: {result}
+دقت کن حدود 8 تا 12 خط باشه توضیحاتت
+لطفا انتهای پست هم رفرنس بزار 
+"""
+        response = await model.generate_content(content)
+        text_ai = response.replace("#", "")
+
+        subscribers = get_subscribers('article_subscribers')
+        for user_id in subscribers:
+            await context.bot.send_message(chat_id=user_id, text=text_ai, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
+
+        # ارسال به کانال آرشیو
+        await context.bot.send_message(chat_id=TARGET, text=text_ai, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
+    
+
+
+
     except Exception as e:
         print(f"ERROR : {e}")
 
