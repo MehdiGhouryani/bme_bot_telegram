01417d88e5ba61811cc8f785134594c088262939
Mehdi <mohammadmahdi670@gmail.com>
2024-12-15 22:09:37 +0330

    optimize

--- main.py
+++ main.py
@@ -43,17 +43,25 @@ genai.configure(api_key=gen_token)
 model = genai.GenerativeModel("gemini-1.5-flash")
 
 
-async def ai_command(update:Update,context:ContextTypes.DEFAULT_TYPE):
+
+
+
+
+async def ai_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if not is_admin(update.message.from_user.id):
         return
-    try:
-        if update.message.reply_to_message:
-            replyText =update.message.reply_to_message.text
+    if update.message.reply_to_message:
+        replyText = update.message.reply_to_message.text
+        try:
             response = model.generate_content(f"""سلام.
-    سوال زیر مربوط به یک کاربر رشته مهندسی پزشکی است. لطفاً پاسخ را به صورت تخصصی و در عین حال به زبان عامیانه و روان فارسی ارائه دهید و از نوشتن مطالب اضافی خودداری کن .\n\n{replyText}""")
-            await update.message.reply_text(response.text,parse_mode=ParseMode.MARKDOWN)
-    except Exception as e:
-        await context.bot.send_message(text=e,chat_id=1717599240)
+
+سوال زیر مربوط به یک کاربر رشته مهندسی پزشکی است. لطفاً پاسخ را به صورت تخصصی و در عین حال به زبان عامیانه و روان فارسی ارائه دهید و از نوشتن مطالب اضافی خودداری کن.\n\n{replyText}""")
+            
+            # اطمینان از اینکه response.text یک رشته است
+            await update.message.reply_text(str(response.text), parse_mode=ParseMode.MARKDOWN)
+        except Exception as e:
+            error_message = f"خطا در تولید محتوا: {str(e)}"
+            await update.message.reply_text(error_message, parse_mode=ParseMode.MARKDOWN)
 
 
 
